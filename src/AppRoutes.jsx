27af633191dbcf93d377410cddb8c105d1acca3e
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2022-12-23 14:43:57 -0600

    Fixed Router

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,44 +1,16 @@
 import { LandingPage, Projects } from './pages';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import { Navbar } from './components';
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Navbar/>
-          <LandingPage />
-        </>
-      ),
-    },
-    {
-      path: "/projects",
-      element: (
-      <>
-        <Navbar/>
-        <Projects />
-      </>
-      ),
-    },
-    {
-      path: "/work",
-      element: (
-      <>
-        <Navbar/>
-      </>
-      ),
-    },
-    {
-      path: "/contact",
-      element: (
-      <>
-        <Navbar/>
-      </>
-      ),
-    },
-]);
-
 export const AppRoutes = () => {
-    return ( <RouterProvider router={router} />)
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path= "/" element= {<><Navbar/><LandingPage /></>}></Route>
+          <Route path= "/projects" element= {<><Navbar/><Projects /></>}></Route>
+          <Route path= "/work" element= {<><Navbar/></>}></Route>
+          <Route path= "/contact" element= {<><Navbar/></>}></Route>
+        </Routes>
+      </BrowserRouter>       
+      )
 }
