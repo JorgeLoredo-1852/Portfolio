516cc19b65603764c4ed2580f3e74ab0078009ca
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-01-02 19:22:26 -0600

    Started Work area

---
+++ src/components/WorkScene/index.jsx
@@ -0,0 +1,156 @@
+import { Environment, OrbitControls, Stars, GizmoHelper, Sky, GizmoViewport, RoundedBox, Text3D, PerspectiveCamera, ScrollControls, useScroll, Box, Plane } from '@react-three/drei'
+import { Model as WorkModel } from "../../models/Work"
+import { Model as RocketModel } from "../../models/Rocket"
+import { Canvas, useFrame } from '@react-three/fiber'
+
+import { Debug, Physics, useBox, usePlane } from '@react-three/cannon'
+import { useRef, useState } from 'react'
+
+export const WorkScene = () => {
+    return(
+        <>
+            <ambientLight intensity={0.5}/>
+            <Environment preset="forest" blur={0.5}/>
+            <group position={[0,0,0]}>
+                <ScrollControls pages={6}>
+                    <Physics gravity={[0, -10, 0]}>
+                        <Debug color="black" scale={1}>
+                            <WorkStation/>
+                        </Debug>
+                    </Physics>
+                </ScrollControls>
+            </group>
+            
+        </>
+    )
+}
+
+function Ground({ color, ...props }) {
+    const [ref] = usePlane(() => ({ ...props }), useRef(null))
+  
+    return (
+      <Plane args={[10, 10]} ref={ref}>
+        <meshStandardMaterial color={color} />
+      </Plane>
+    )
+  }
+const Crate = ({position}) => {
+    const [ref, api] = useBox(() => ({ args: [2, 1, 1], mass: 1, position }), useRef(null))
+    const scroll = useScroll()
+    const [once, setOnce] = useState(true)
+
+    useFrame((state, delta) => {
+        const offset = scroll.offset
+        
+        if(offset > 0.16 && once){
+            setOnce(false)
+            api.applyImpulse([0, 5, 2], [0, -1, 0])
+        }
+    })
+
+    return (
+      <Box
+        args={[2, 1, 1]}
+        ref={ref}
+      >
+        <meshNormalMaterial />
+      </Box>
+    )
+  }
+
+const WorkStation = () => {
+    const scroll = useScroll()
+    
+    const [posRocket, setPosRocket] = useState([0,0,0])
+    const [rotRocket, setRotRocket] = useState([0,0,0])
+    const [hit, setHit] = useState(false)
+    
+    useFrame((state, delta) => {
+        const offset = scroll.offset
+        
+        state.camera.position.set(0, 0, 400 - offset * 500)
+        setPosRocket([0, -0.4, 400 - offset * 500 - 2])
+
+        state.camera.rotation.set(0,0,0)
+        setRotRocket([0,0,-Math.PI * offset])
+
+        if(offset > 0.16){
+            setHit(true)
+        }
+    })
+
+
+    
+    return (
+        <>
+
+                    <RocketModel position={posRocket} rotation={[-Math.PI/2 + Math.PI / 20,0,0]}/>
+                    <group rotation={rotRocket}>
+                        {/*<WorkModel position={[0,0,0]} rotation={[0,0,0]}/>*/}
+                        
+                        {!hit && <Ground color="grey" position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} />}
+        
+                        <Crate position={[2, 0, 0]} />
+                        <Crate position={[0, 0, 0]} />
+                        <Crate position={[-2, 0, 0]} />
+
+                        <Stars radius={50} depth={50} count={5000} factor={20} saturation={0} fade speed={1}/>
+                    </group>
+
+        </>
+    )
+}
+
+/*
+
+const WorkStation = () => {
+    const scroll = useScroll()
+    
+    const [posStation, setPosStation] = useState([0,0,0])
+    const [rStation, setRStation] = useState([0,0,0])
+
+    const [lastPos, setLastPos] = useState([0,0,0])
+    const [lastPos2, setLastPos2] = useState([0,0,0])
+    const [lastRt, setLastRt] = useState([0,0,0])
+    
+    useFrame((state, delta) => {
+        const offset = scroll.offset
+        if(offset <= 0.13){
+            //if(offset <= 0.1){
+            //    state.camera.position.set(-offset*200,40 - offset*300, 80 - offset*300)
+            //} else {
+            //    state.camera.position.set(-offset*200,40 - offset*350, 80 - offset*300)
+            //}
+            setPosStation([offset * 40, offset * 95, offset*150])
+            setLastPos([offset * 40,offset * 95,offset*150])
+
+            setRStation([-Math.PI/8 - offset,offset*2,0])
+            setLastRt([-Math.PI/8 - offset,offset*2,0])
+            //setLastPos([-offset*200, 50 - offset*300, 100 - offset*300])
+            //state.camera.lookAt(0, 0, 0)
+            //state.camera.lookAt(0, 0, 5)
+            //state.camera.rotateY(-0.002)
+        } else if(offset <= 0.2){
+            setPosStation([lastPos[0] - offset*5, lastPos[1], lastPos[2]])
+            setLastPos2([lastPos[0] - offset*5, lastPos[1], lastPos[2]])    
+        } else if(offset <= 0.333) {
+            console.log("AAAAAA")
+            console.log(offset)
+            setPosStation([lastPos[0] - offset * 30, lastPos2[1] , lastPos2[2]])
+
+            //setLastPos([offset * 40,offset * 95,offset*150])
+            //setRStation([-Math.PI/8 - offset,offset*2,0])
+            //state.camera.position.set(lastPos[0] + offset*100, lastPos[1], lastPos[2])
+            //state.camera.lookAt(0, 0, 0)
+        }
+        //state.camera.position.set(Math.sin(offset) * -10, Math.atan(offset * Math.PI * 2) * 5, Math.cos((offset * Math.PI) / 3) * -10)
+    })
+    
+    return (
+        <>
+            <WorkModel position={posStation} rotation={rStation}/>
+        </>
+    )
+}
+
+*/
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,4 +1,27 @@
+import { Grid } from "@mui/material"
+import { WorkScene } from '../components'
+import { Canvas } from "@react-three/fiber"
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useInView } from "react-intersection-observer";
+
+import { useState, useRef, useEffect } from "react";
+
 
 export const Work = () => {
-    return(<div>Work</div>)
+    return(
+
+    <div style={{width:"100vw", height:"100vh", backgroundColor:"#000"}} >
+        <Canvas 
+            className='canvas' 
+            shadows
+            camera={{position: [0,30,60], fov:20}}
+            color={"#000000"}
+            style={{zIndex:100}}
+        >
+            <WorkScene/>
+        </Canvas>
+    </div>
+
+    )
 }
