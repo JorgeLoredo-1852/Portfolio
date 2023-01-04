9c225b32fdbf6c1d1327c584e74d9a84b2ddea72
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-01-04 16:22:07 -0600

    Changed Dir name

--- src/models/technologies/c.jsx
+++ src/models/technologies/c.jsx
@@ -6,7 +6,7 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF("/3DTech/c++.glb");
+  const { nodes, materials } = useGLTF("/3DTech/c.glb");
   return (
     <group {...props} dispose={null}>
       <group position={[0, 1.02, 0]} scale={[1, 0.5, 1]}>
@@ -39,4 +39,4 @@ export function Model(props) {
   );
 }
 
-useGLTF.preload("/3DTech/c++.glb");
+useGLTF.preload("/3DTech/c.glb");
