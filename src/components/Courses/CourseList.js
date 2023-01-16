be04f0ac85f0cd1fd7346b5e25e4326394270fde
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-01-16 11:39:00 -0600

    Added table

---
+++ src/components/Courses/CourseList.js
@@ -0,0 +1,83 @@
+export const courseList = [
+    {
+        id:1,
+        name:"Node.js, Express, MongoDB & More: The Complete Bootcamp 2023",
+        area:"Backend",
+        by:"Jonas Schmedtmann",
+        platform:"Udemy",
+        date:"March 2021",
+        link:"https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/",
+        image:""
+    },
+    {
+        id:2,
+        name:"Advanced CSS and Sass: Flexbox, Grid, Animations and More!",
+        area:"Frontend",
+        by:"Jonas Schmedtmann",
+        platform:"Udemy",
+        date:"March 2021",
+        link:"https://www.udemy.com/course/advanced-css-and-sass/",
+        image:""
+    },
+    {
+        id:3,
+        name:"Modern React with Redux [2023 Update]",
+        area:"Frontend",
+        by:"Stephen Grider",
+        platform:"Udemy",
+        date:"Jan 2021",
+        link:"https://www.udemy.com/course/advanced-css-and-sass/",
+        image: ""
+    },
+    {
+        id:8,
+        name:"HTML, CSS, Javascript for Web Developers",
+        area:"Frontend",
+        by:"John Hopkins University",
+        platform:"Coursera",
+        date:"Dec 2020",
+        link:"https://mx.coursera.org/learn/html-css-javascript-for-web-developers",   
+        image:""
+    },
+    {
+        id:6,
+        name:"Tensorflow Data and Deployment",
+        area:"ML/AI",
+        by:"Deep Learning.AI",
+        platform:"Coursera",
+        date:"Sept 2020",
+        link:"https://mx.coursera.org/specializations/tensorflow-data-and-deployment",   
+        image:""
+    },
+    {
+        id:7,
+        name:"Deep Learning.AI Tensorflow Developer",
+        area:"ML/AI",
+        by:"Deep Learning.AI",
+        platform:"Coursera",
+        date:"Sept 2020",
+        link:"https://mx.coursera.org/professional-certificates/tensorflow-in-practice",   
+        image:""
+    },
+    {
+        id:4,
+        name:"Deep Learning Specialization",
+        area:"ML/AI",
+        by:"Deep Learning.AI",
+        platform:"Coursera",
+        date:"Aug 2020",
+        link:"https://mx.coursera.org/specializations/deep-learning",  
+        image:"" 
+    },
+    {
+        id:5,
+        name:"Python for Everybody",
+        area:"Algorithms",
+        by:"University of Michigan",
+        platform:"Coursera",
+        date:"May 2020",
+        link:"https://mx.coursera.org/specializations/python",   
+        image:""
+    },
+
+]
--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,6 +1,86 @@
-import { useState, useRef, useEffect } from 'react'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Tab } from '@mui/material';
+
+import { courseList } from './CourseList';
+import { groupList } from './Groups';
 
 export const Courses = () => {
-    return(<>
-    Courses</>)
+    return(
+    <>
+        <TableGroups/>
+        <TableCourses/>
+    </>)
 }
+
+
+
+function TableGroups() {
+  return (
+    <TableContainer component={Paper} sx={{marginTop:"1rem"}}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align='left' style={{width:50}} >Img</TableCell>
+            <TableCell align='left'>Name</TableCell>
+            <TableCell align='left' style={{width:175}}>Area</TableCell>
+            <TableCell align='left' style={{width:175}}>Role</TableCell>
+            <TableCell align='left' style={{width:175}}>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {groupList.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{row.image}</TableCell>
+              <TableCell align='left'>{row.name}</TableCell>
+              <TableCell align='left'>{row.area}</TableCell>
+              <TableCell align='left'>{row.role}</TableCell>
+              <TableCell align='left'>{row.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+function TableCourses() {
+    return (
+      <TableContainer component={Paper} sx={{marginTop:"1rem"}}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell component="th" scope="row" style={{width:50}}>Img</TableCell>
+              <TableCell align='left'>Name</TableCell>
+              <TableCell align='left' style={{width:175}}>Area</TableCell>
+              <TableCell align='left' style={{width:175}}>By</TableCell>
+              <TableCell align='left' style={{width:175}}>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {courseList.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{row.image}</TableCell>
+                <TableCell align='left'>{row.name}</TableCell>
+                <TableCell align='left'>{row.area}</TableCell>
+                <TableCell align='left'>{row.by}</TableCell>
+                <TableCell align='left'>{row.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
