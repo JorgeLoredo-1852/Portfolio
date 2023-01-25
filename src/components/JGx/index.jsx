97d31846d7d564b58d13d7a0f7e7af7632a5ae31
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-01-24 23:25:37 -0600

    Added cv download

--- src/components/JGx/index.jsx
+++ src/components/JGx/index.jsx
@@ -7,7 +7,7 @@ import Twitter from '../../assets/icons/Twitter.png';
 import Instagram from '../../assets/icons/Instagram.png';
 import Linkedin from '../../assets/icons/Linkedin.png';
 import Github from '../../assets/icons/Github.png';
-
+import Download from '../../assets/icons/Download.png';
  
 export const JGx = ({notHide = true}) => {
     const themeM = useTheme();
@@ -23,6 +23,20 @@ export const JGx = ({notHide = true}) => {
         setDisplayLinks(!displayLinks)
     }
 
+    const downloadCV = () => {
+        fetch('/Jorge_Loredo_CV.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = '/Jorge_Loredo_CV.pdf';
+                alink.click();
+            })
+        })
+    }
+
 
   return (
     <div  className={notHide ? "navbar" : "navbar navbar--hide"} style={{flexDirection:downSm ? "column" : "row"}}>
@@ -30,6 +44,12 @@ export const JGx = ({notHide = true}) => {
             <img className="navbar--header" style={{userSelect:"none"}} src={JGxLogo}/>
         </div>
         <div style={{ display:displayLinks ? "flex" : "none", flexDirection: downSm ? "column" : "row"}} className={downSm? "linksNavTop" : "linksNavLeft"}>
+        <div style={{display: displayLinks ? "flex" : "none", cursor:"pointer", backgroundColor:"#3900BB", borderRadius:"100%", alignItems:"center", justifyContent:"center", marginLeft: downSm ? "0" : "1rem", zIndex:"180", marginTop: downSm ? "1rem" : "0"}} className="nav--link">
+                <div onClick={downloadCV} style={{width:"4.1rem", height:"4.1rem"}}>
+                    <img className="navbar--item" src={Download}/>
+                </div>
+            </div> 
+            
             <div style={{display: displayLinks ? "flex" : "none", backgroundColor:"#3900BB", borderRadius:"100%", alignItems:"center", justifyContent:"center", marginLeft: downSm ? "0" : "1rem", zIndex:"180", marginTop: downSm ? "1rem" : "0"}} className="nav--link">
                 <a href={"https://twitter.com/JGxFazciste"} target="_blank" rel='nonereferrer' style={{width:"4.1rem", height:"4.1rem"}}>
                     <img className="navbar--item" src={Twitter}/>
